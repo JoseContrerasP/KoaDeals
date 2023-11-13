4b0654c47af14b8a71155e2969b03c29a70dce8a
Jose-Contreras <104838758+JoseContrerasP@users.noreply.github.com>
2023-11-13 12:19:54 -0400

    production-ready

--- manage.py
+++ manage.py
@@ -3,10 +3,16 @@
 import os
 import sys
 
+from settings import base
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KoaDeals.settings')
+    if base.DEBUG:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +24,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+import dj_database_url
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/mysite", conn_max_age=600
+    )
+}
