31efbe54527d8094daf9a8b8d0a4140030f6c056
Jose-Contreras <104838758+JoseContrerasP@users.noreply.github.com>
2023-11-14 13:27:11 -0400

    add settings file

--- KoaDeals/settings.py
+++ KoaDeals/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import os
 import environ
+import dj_database_url
 
 env = environ.Env()
 environ.Env().read_env()
@@ -16,6 +17,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -69,6 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "KoaDeals.wsgi.application"
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/mysite", conn_max_age=600
+    )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- manage.py
+++ manage.py
@@ -3,16 +3,10 @@
 import os
 import sys
 
-from settings import base
-
 
 def main():
     """Run administrative tasks."""
-    if base.DEBUG:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "koadeals.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
