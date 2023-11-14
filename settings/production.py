813137a2ca95a519c22b540dbfc18bd9717bf8f3
Jose-Contreras <104838758+JoseContrerasP@users.noreply.github.com>
2023-11-13 23:11:00 -0400

    add createsuperuser

--- settings/production.py
+++ settings/production.py
@@ -1,8 +1,6 @@
 from .base import *
 import dj_database_url
 
-ALLOWED_HOSTS = []
-
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
